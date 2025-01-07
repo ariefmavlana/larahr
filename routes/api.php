385b84f15563feb5c5337a173f95b12eafd42b40
ariefmavlana <ariefmaulana0808@gmail.com>
2025-01-07 17:02:07 +0700

    API: User

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CompanyController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\API\UserController;
 
 Route::get('/company', [CompanyController::class, 'all']);
+
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('user', [UserController::class, 'fetch'])->middleware('auth:sanctum');
